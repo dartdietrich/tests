c785816dad9a961f8421ded9aa43ef18a905fd3e
Edgar 'dietrich' Januschevski <edgars@pharmidea.lv>
2012-06-28 14:26:43 +0300

    sākums

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lapa::Application.config.secret_token = '9bba8b044c8d19a023964e4703527c5cfbb963383293f758ea19c27376f732bcbed9ad3e265ee7b6e710d9874799583e4fb2a9b08b2aa79fa5df853b3ed65d01'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Lapa::Application.config.session_store :cookie_store, key: '_lapa_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Lapa::Application.config.session_store :active_record_store
